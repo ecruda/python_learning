75d047c61613e752df3f795529bee2a56d4589b6
ecruda <ecruda@mail.smu.edu>
2022-04-05 02:06:10 -0500

    fixed py_class and py_obj

--- obj_and_classes/py_class.py
+++ obj_and_classes/py_class.py
@@ -11,18 +11,18 @@ class Car:
 
 	#increase speed, cannot be increased unless car is started
 	def inc_speed(self, delta):
-		if self.started
+		if self.started:
 			self.speed = self.speed + delta
-			print("Speed is now" + self.speed)
+			print("Speed is now " + str(self.speed))
 
 		else:
 			print("Must start car first")
 
 	#decrease speed, cannot be dec until car started
 	def dec_speed(self, delta):
-		if self.started
+		if self.started:
 			self.speed = self.speed - delta
-			print("Speed is now" + self.speed)
+			print("Speed is now " + str(self.speed))
 
 		else:
 			print("Must start car first")
--- obj_and_classes/py_obj.py
+++ obj_and_classes/py_obj.py
@@ -1,5 +1,7 @@
 #py_obj.py
-import Car as c
+import py_class
 
-car1 = c()
+car1 = py_class.Car()
 car1.start()
+car1.inc_speed(30)
+car1.dec_speed(50)
